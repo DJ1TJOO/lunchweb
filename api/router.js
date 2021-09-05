56bd8c10f18283f7ec17c5654ce50942a20feb8f
DJ1TJOO <t.ferb1@gmail.com>
2021-09-05 12:56:27 +0200

    Added products api to api router

--- api/router.js
+++ api/router.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/users", require("./users"));
+router.use("/products", require("./products"));
 
 module.exports = router;
