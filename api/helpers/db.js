476016ede64a865e98217c48dd2c37be595ed727
DJ1TJOO <t.ferb1@gmail.com>
2021-09-08 20:38:38 +0200

    Changed db to env

--- api/helpers/db.js
+++ api/helpers/db.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2");
 
 // Make connection to db and create pool
 const configuration = {
-	host: "localhost",
-	user: "root",
-	database: "lunchweb",
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASS,
+	database: process.env.MYSQL_DB,
 	dateStrings: true,
 };
 module.exports = mysql.createPool(configuration).promise();
